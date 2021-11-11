a3587638d44326e2f38f33d80b6cc37aa3c0b57b
fedor.krasin <fedor.krasin@itechart-group.com>
2021-11-11 10:23:36 +0300

    Added player's rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,18 +5,30 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private float _rotationSpeed;
+
+    private Vector2 _rotation;
     
     private void Update()
     {
         Move();
+        Rotate();
     }
 
     private void Move()
     {
         var vertical = _speed * Input.GetAxis("Vertical");
         var horizontal = _speed * Input.GetAxis("Horizontal");
-        
-        transform.position += Vector3.back * vertical * Time.deltaTime;
+
+        var destinationVector = new Vector3(transform.forward.x, 0, transform.forward.z).normalized;
+        transform.position += destinationVector * vertical * Time.deltaTime;
         transform.position += Vector3.left * horizontal * Time.deltaTime;
+    }
+
+    private void Rotate()
+    {
+        _rotation.x += Input.GetAxis("Mouse X") * _rotationSpeed;
+        _rotation.y += Input.GetAxis("Mouse Y") * _rotationSpeed;
+        transform.localRotation = Quaternion.Euler(-_rotation.y, _rotation.x, 0);
     }
 }
