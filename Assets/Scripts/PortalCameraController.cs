ef808bbc20900435c381b7efebb5d48db2d01b22
fedor.krasin <fedor.krasin@itechart-group.com>
2021-11-11 09:47:56 +0300

    Created portal camera script

---
+++ Assets/Scripts/PortalCameraController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCameraController : MonoBehaviour
+{
+    [SerializeField] private PortalCameraController _otherPortal;
+    [SerializeField] private Camera _portalView;
+
+    private void Update()
+    {
+        SetPortalView();
+    }
+
+    private void SetPortalView()
+    {
+        var playerPosition = _otherPortal.transform.worldToLocalMatrix.MultiplyPoint(Camera.main.transform.position);
+        _portalView.transform.localPosition = -playerPosition;
+    }
+}
