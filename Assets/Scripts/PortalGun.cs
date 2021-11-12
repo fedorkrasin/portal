2e2f8bc24e6128c9a51e761268c02a28c3146301
fedor.krasin <fedor.krasin@itechart-group.com>
2021-11-12 11:37:04 +0300

    Added portal gun

--- Assets/Scripts/PortalGun.cs
+++ Assets/Scripts/PortalGun.cs
@@ -4,6 +4,39 @@ using UnityEngine;
 
 public class PortalGun : MonoBehaviour
 {
-    [SerializeField] private GameObject _firstPortal;
-    [SerializeField] private GameObject _secondPortal;
+    [SerializeField] private PortalTeleporter _firstPortal;
+    [SerializeField] private PortalTeleporter _secondPortal;
+
+    [SerializeField] private float _maxDistance;
+    [SerializeField] private LayerMask _hitableLayer;
+    [SerializeField] private float _groundOffset;
+
+    private void Update()
+    {
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        {
+            var ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+            RaycastHit hit;
+            
+            if (Physics.Raycast(ray, out hit, _maxDistance, _hitableLayer))
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    _firstPortal.transform.rotation = Quaternion.LookRotation(hit.normal);
+                    _firstPortal.transform.position = hit.point + _firstPortal.transform.forward * _groundOffset;
+                }
+                
+                if (Input.GetMouseButtonDown(1))
+                {
+                    _secondPortal.transform.rotation = Quaternion.LookRotation(hit.normal);
+                    _secondPortal.transform.position = hit.point + _secondPortal.transform.forward * _groundOffset;
+                }
+            }
+        }
+    }
 }
